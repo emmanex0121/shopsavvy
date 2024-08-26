1c655c7c64e6000c2e2efa1f2321173d189350c7
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-08-26 11:19:18 +0000

    Routes

--- frontend/src/common/components/ui/RegisterForm.jsx
+++ frontend/src/common/components/ui/RegisterForm.jsx
@@ -23,14 +23,67 @@ const LoginForm = () => (
     <h1 className="text-center text-2xl font-bold mb-10">Welcome Back</h1>
     <p className="text-center mb-10">Please enter your details to sign in</p>
 
+    <div className="flex flex-wrap gap-4">
+      <Form.Item
+        className="flex-1 min-w-[200px]"
+        name="firstName"
+        rules={[
+          {
+            required: true,
+            message: "Please input your First Name",
+          },
+        ]}>
+        <label className="block text-sm font-bold text-gray-700">
+          First Name <span className="text-customOrange">*</span>
+        </label>
+        <Input
+          className="mt-1 bg-customGrey border-black"
+          placeholder="First Name"
+        />
+      </Form.Item>
+
+      <Form.Item
+        className="flex-1 min-w-[200px]"
+        name="lastName"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Last Name",
+          },
+        ]}>
+        <label className="block text-sm font-bold text-gray-700">
+          Last Name <span className="text-customOrange">*</span>
+        </label>
+        <Input
+          className="mt-1 bg-customGrey border-black"
+          placeholder="Last Name"
+        />
+      </Form.Item>
+    </div>
+
     <Form.Item
-      // label="Email"
+      className=""
+      name="userName"
+      rules={[
+        {
+          required: true,
+          message: "Please input your First Name",
+        },
+      ]}>
+      <label className="block text-sm font-bold text-gray-700">
+        User Name <span className="text-customOrange">*</span>
+      </label>
+      <Input className="mt-1 bg-customGrey border-black" placeholder="" />
+    </Form.Item>
+
+    <Form.Item
+      className=""
       name="email"
       rules={[
         {
           required: true,
           type: "email",
-          message: "Please input your email!",
+          message: "Please input your First Name",
         },
       ]}>
       <label className="block text-sm font-bold text-gray-700">
@@ -51,7 +104,7 @@ const LoginForm = () => (
           message: "Please input your password!",
         },
       ]}>
-      <label className="block text-sm mt-5 font-bold text-gray-700">
+      <label className="block text-sm font-bold text-gray-700">
         Password <span className="text-customOrange">*</span>
       </label>
       <Input.Password
--- frontend/src/views/Login/Login.jsx
+++ frontend/src/views/Login/Login.jsx
@@ -4,22 +4,14 @@ import bgImage from "../../assets/images/bg-cart-large.jpg";
 import SavyyLogo from "../../common/components/ui/SavyyLogo";
 
 const Login = () => {
-  // const onFinish = (values) => {
-  //   console.log("Success:", values);
-  // };
-
-  // const onFinishFailed = (errorInfo) => {
-  //   console.log("Failed:", errorInfo);
-  // };
-
   return (
     <div
       className="h-full absolute inset-0 bg-cover bg-center"
       style={{
         backgroundImage: `url(${bgImage})`,
       }}>
-      <section className="w-full md:w-1/2 h-[100vh] fixed right-0 bg-black flex flex-col items-center">
-        <SavyyLogo width="30vh" /> 
+      <section className="w-full md:w-1/2 h-[100vh] fixed right-0 bg-black flex flex-col items-center px-5">
+        <SavyyLogo width="30vh" />
         <LoginForm />
       </section>
     </div>
